6a89818a305ce09a1dc20749093ab4669ce0f1b0
Amandeep <amanscollection@gmail.com>
2025-09-23 12:21:30 +0530

    Added Files

--- learn-terraform-get-started-aws/main.tf
+++ learn-terraform-get-started-aws/main.tf
@@ -14,10 +14,12 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.mirco"
+  ami = data.aws_ami.ubuntu.id
+  # instance_type = "t2.mirco"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "learn-terraform"
+    # Name = "learn-terraform"
+    Name = var.instance_name
   }
 }
